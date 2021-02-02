c279ad56c80a35645d1fc4e69d0d684d17639723
jotabraham <jotabraham@gmail.com>
2021-02-01 22:07:19 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// #1
+let total = 0;
+let totalParagraph = document.querySelector(".total");
+let cola = document.querySelector(".cola");
+let peanuts = document.querySelector(".peanuts");
+let chocolate = document.querySelector(".chocolate");
+let gummies = document.querySelector(".gummies");
+
+cola.addEventListener("click", () => {
+  total += 2;
+  totalParagraph.textContent = `Total: $${total}`;
+});
+peanuts.addEventListener("click", () => {
+  total += 3;
+  totalParagraph.textContent = `Total: $${total}`;
+});
+chocolate.addEventListener("click", () => {
+  total += 4;
+  totalParagraph.textContent = `Total: $${total}`;
+});
+gummies.addEventListener("click", () => {
+  total += 6;
+  totalParagraph.textContent = `Total: $${total}`;
+});
+
+// #2
+let coinsContainer = document.querySelector(".coins-container");
+let moneyForm = document.querySelector(".money-form");
+
+moneyForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let formData = new FormData(moneyForm);
+  let howMany = formData.get("number");
+  let coinType = formData.get("coin");
+  for (let i = 1; i <= howMany; i++) {
+    coinsContainer.append(coinType);
+  }
+});
